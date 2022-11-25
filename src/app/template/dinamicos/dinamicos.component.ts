b9ecc4999fe48e5579221d114d3a832b6ba089ac
JFelipex3 <jfelipe.machuca@gmail.com>
2022-11-24 23:07:50 -0300

    Formularios Dinámicos
    Formularios Dinámicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  persona: Persona = {
+    nombre: "Jhonnatan",
+    favoritos: [
+      { id: 1, nombre: 'Horizon Zero Dawn'},
+      { id: 2, nombre: 'Call of Duty Modern Warfare 2'},
+    ]
+  }
 
-  ngOnInit(): void {
+  guardar(){
+    console.log("Formulario Posteado")
   }
 
 }
