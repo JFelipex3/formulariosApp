3bb05a5a5fea3b20c3d1cd1b74033343f9eeaa45
JFelipex3 <jfelipe.machuca@gmail.com>
2022-11-27 13:03:06 -0300

    Fin Formularios Template
    Fin Formularios Template
    - Dinámicos
    - Switches

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -18,12 +18,28 @@ interface Favorito {
 })
 export class DinamicosComponent {
 
+  nuevoJuego: string = '';
+
   persona: Persona = {
     nombre: "Jhonnatan",
     favoritos: [
       { id: 1, nombre: 'Horizon Zero Dawn'},
       { id: 2, nombre: 'Call of Duty Modern Warfare 2'},
     ]
+  }
+
+  agregarJuego(){
+    const nuevoFavorito: Favorito = { 
+      id : this.persona.favoritos.length,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+  }
+
+  eliminar( index: number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
   guardar(){
